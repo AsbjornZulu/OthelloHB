dd1ffa4c2fc838915739d4b6e3234e7b1b0b2553
AsbjornZulu <51274631+AsbjornZulu@users.noreply.github.com>
2022-12-24 21:27:07 +0100

    Add files via upload

---
+++ OthelloHB/src/edu/upc/epsevg/prop/othello/players/hb/InfoNode.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.othello.players.hb;
+
+/**
+ *
+ * @author Eric
+ */
+public class InfoNode {
+    //Tal qual com a classe;
+    byte indexMillorFill;
+    long num1, num2;
+    
+    //public InfoNode(byte indexMillorFill, MyGameStatus myGameStatus) {
+        //this.indexMillorFill = indexMillorFill;
+        //this.num1 = myGameStatus.getBoard_occupied().toLongArray()[0];
+        //this.num2 = myGameStatus.getBoard_color().toLongArray()[0];
+    //}
+}
